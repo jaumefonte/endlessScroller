23854594315d2f9c285a0f469f0189a4635bf424
Jaume Fonte <jaume.fonte@gmail.com>
2022-09-16 12:01:38 +0200

    Obstacle pooling

--- Assets/ObstacleGenerator.cs
+++ Assets/ObstacleGenerator.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Pool;
 public class ObstacleGenerator : MonoBehaviour
 {
     [SerializeField] List<GameObject> obstaclesPrefabs;
@@ -9,10 +9,40 @@ public class ObstacleGenerator : MonoBehaviour
     float generationTimer;
     int obstacleIndex = 0;
     [SerializeField] Transform spawner;
-    void Start()
+    public IObjectPool<GameObject> obstaclePool;
+    [SerializeField] Destroyer obstacleDestroyer;
+    private void Awake()
     {
-        
+        obstaclePool = new ObjectPool<GameObject>(GenerateObstacle, GetObstacle, ReleaseObstacle, DestroyObstacle, true, 20, 20);
+        obstacleDestroyer.SetPool(obstaclePool);
     }
+    GameObject GenerateObstacle()
+    {
+        GameObject currentObstacle = obstaclesPrefabs[Random.Range(0, obstaclesPrefabs.Count)];
+        Vector3 obstaclePosition = new Vector3(spawner.position.x, currentObstacle.transform.position.y, currentObstacle.transform.position.z);
+        GameObject currentInstance = GameObject.Instantiate(currentObstacle, obstaclePosition, Quaternion.identity);
+        currentInstance.transform.SetParent(transform);
+        return currentInstance;
+    }
+    private void GetObstacle(GameObject obj)
+    {
+        obj.SetActive(true);
+        obj.transform.position = new Vector3(spawner.position.x,obj.transform.position.y, obj.transform.position.z);
+    }
+    private void ReleaseObstacle(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+    private void DestroyObstacle(GameObject obj)
+    {
+        Destroy(obj);
+    }
+
+    
+
+   
+
+
 
     // Update is called once per frame
     void Update()
@@ -24,18 +54,11 @@ public class ObstacleGenerator : MonoBehaviour
         else
         {
             obstacleIndex++;
-            GenerateObstacle();
+            obstaclePool.Get();
             ResetTimer();
         }
     }
-    void GenerateObstacle()
-    {
-        GameObject currentObstacle = obstaclesPrefabs[Random.Range(0, obstaclesPrefabs.Count)];
-        Vector3 obstaclePosition = new Vector3(spawner.position.x, currentObstacle.transform.position.y, currentObstacle.transform.position.z);
-        GameObject currentInstance = GameObject.Instantiate(currentObstacle, obstaclePosition, Quaternion.identity);
-        currentInstance.transform.SetParent(transform);
-
-    }
+    
     void ResetTimer()
     {
         generationTimer = generationInterval;
--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Destroyer : MonoBehaviour
 {
-   
-    private void OnCollisionEnter2D(Collision2D collision)
+    IObjectPool<GameObject> obstaclePool;
+    public void SetPool(IObjectPool<GameObject> currentPool)
     {
-        if (collision.collider.tag == "Player")
+        obstaclePool = currentPool;
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
         {
             Debug.Log("------------ PLAyER");
-            Destroy(collision.collider.gameObject);
+            Destroy(collider.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("------------ OBSTACLE "+collider.name);
+        if (collider.tag == "Obstacle")
+        {
+            Debug.Log("------------ TAG");
+            //Destroy(collider.transform.parent.gameObject);
+            obstaclePool.Release(collider.transform.parent.gameObject);
         }
     }
 }
