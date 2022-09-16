339f4b27a0dad579b217ad95810fe51fdb64fadc
Jaume Fonte <jaume.fonte@gmail.com>
2022-09-16 10:57:58 +0200

    background scroll

--- Assets/Scripts/BGScroll.cs
+++ Assets/Scripts/BGScroll.cs
@@ -13,7 +13,12 @@ public class BGScroll : MonoBehaviour
     {
         for (int i = 0; i < renderers.Count; i++)
         {
+            if (renderers[i].transform.position.x < -22)
+            {
+                renderers[i].transform.position = new Vector3(24f, renderers[i].transform.position.y, renderers[i].transform.position.z);
+            }
             renderers[i].transform.position = new Vector3(renderers[i].transform.position.x - bgVelocity * Time.deltaTime, renderers[i].transform.position.y, renderers[i].transform.position.z);
+            
         }
 
     }
